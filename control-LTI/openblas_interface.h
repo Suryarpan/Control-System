03a34f87b3b2170a1f291ce7d375ea4313df7e20
Suryarpan Chowdhury <pupu@localhost.localdomain>
2019-09-22 13:59:55 +0530

    added functions

--- control-LTI/openblas_interface.h
+++ control-LTI/openblas_interface.h
@@ -1,6 +1,29 @@
 #ifndef OPENBLAS_INTERFACE_H
 #define OPENBLAS_INTERFACE_H
 
+#include <stdbool.h>
+#include "cblas.h"
 
+struct mtrx
+{
+    int row;
+    int col;
+    bool owner;
+    double *elem;
+};
+
+typedef struct mtrx mtrx;
+
+mtrx * mtrx_alloc( int, int, bool, double * );
+
+void mtrx_free( mtrx * );
+
+mtrx * mtrx_add( mtrx *, mtrx * );
+
+mtrx * mtrx_sub( mtrx *, mtrx * );
+
+mtrx * mtrx_nmul( double, mtrx * );
+
+mtrx * mtrx_mmul( mtrx *, mtrx * );
 
 #endif // !OPENBLAS_INTERFACE_H
