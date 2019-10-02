74d875ff75c9a3ebecc26b38b7e4e4d4bfb3f3ef
Suryarpan Chowdhury <pupu@localhost.localdomain>
2019-10-02 22:19:55 +0530

    addition of functions

--- control-LTI/openblas_interface.h
+++ control-LTI/openblas_interface.h
@@ -1,8 +1,8 @@
 #ifndef OPENBLAS_INTERFACE_H
 #define OPENBLAS_INTERFACE_H
 
-#include <stdlib.h>
 #include <stdbool.h>
+
 #include "cblas.h"
 
 struct mtrx
@@ -17,11 +17,13 @@ typedef struct mtrx mtrx;
 
 mtrx * mtrx_alloc( int, int, bool, double * );
 
+mtrx * mtrx_init_unit( int, bool );
+
 void mtrx_free( mtrx * );
 
 mtrx * mtrx_add( mtrx *, mtrx * );
 
-mtrx * mtrx_sub( mtrx *, mtrx * );
+mtrx * mtrx_add_unit( mtrx * );
 
 mtrx * mtrx_nmul( double, mtrx * );
 
